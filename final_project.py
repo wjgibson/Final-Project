9094770387f49385b17476d8dc920bdd6e85a8b8
wjgibson <wjgibson@bsu.edu>
2022-04-25 12:26:36 -0400

    Complete tests. All seven pass

--- final_project.py
+++ final_project.py
@@ -57,12 +57,13 @@ class DAO():
         :return: Number of successful insertions
         :rtype: int
         """
-        if self.is_stub:
-            return len(batch)
         
         if type(batch) is str:
             print("Incorrect parameter type: should be a list of messages")
             return -1
+
+        if self.is_stub:
+            return len(batch)
         
         inserted = 0
         
@@ -113,27 +114,38 @@ class DAO():
         return deleted
 
     def read_recent_position_given_MMSI(self, MMSI):
+        length_check = str(MMSI)
+        if len(length_check) != 9:
+            print("Error: MMSI must be a 9 digit number. Returning empty string...")
+            return ""
+        
         query = """
         select timestamp, MMSI, latitude, longitude, Vessel_IMO 
         from ais_message, position_report 
         where position_report.AISMessage_Id=ais_message.id and MMSI={} order by timestamp limit 1;""".format(MMSI)
 
         document = self.run(query)
-        return document
+        results = [tuple(str(item) for item in t) for t in document]
+        #print(results)
+        return results
 
 
 
+    #This will not have the limit of 1 once we use the new database. The test database has too many records to not have the limit
+    #Remove "limit 100" before submitting!
     def read_all_recent_ship_positions(self):
         query = """
         select MMSI, latitude, longitude, Vessel_IMO 
         from ais_message, position_report 
-        where position_report.AISMessage_Id=ais_message.id order by timestamp limit 1;"""
+        where position_report.AISMessage_Id=ais_message.id order by timestamp limit 100;"""
 
         document = self.run(query)
-        return document
+        results = [tuple(str(item) for item in t) for t in document]
+        return results
 
     #Help? What does this query look like? My attempts give me an empty set
-    #Attempt: select MMSI, latitude, longitude, AISIMO, name from ais_message, position_report, static_data where ais_message.id=position_report.AISMessage_Id and position_report.AISMessage_id=static_data.AISMessage_Id limit 10;
+    #Attempt: select MMSI, latitude, longitude, AISIMO, name from ais_message, position_report, static_data 
+    # where ais_message.id=position_report.AISMessage_Id and position_report.AISMessage_id=static_data.AISMessage_Id limit 10;
     #results in an empty set?
     def read_vessel_info(self, MMSI, IMO, name):
         pass
@@ -148,7 +160,20 @@ class DAO():
         pass
 
     def read_last_five_positions_given_MMSI(self, MMSI):
-        pass
+        length_check = str(MMSI)
+        if len(length_check) != 9:
+            print("Error: MMSI must be a 9 digit number. Returning empty string...")
+            return ""
+        
+        query = """
+        select timestamp, MMSI, latitude, longitude, Vessel_IMO 
+        from ais_message, position_report 
+        where position_report.AISMessage_Id=ais_message.id and MMSI={} order by timestamp limit 5;""".format(MMSI)
+
+        document = self.run(query)
+        results = [tuple(str(item) for item in t) for t in document]
+        #print(results)
+        return results
 
     def read_recent_ship_positions_headed_to_port_ID(self, port_id):
         pass
@@ -219,8 +244,6 @@ class StaticData( Message ):
 
 class TMB_test(unittest.TestCase):
     
-    tmb = DAO(True)
-    
     batch = """[ {\"Timestamp\":\"2020-11-18T00:00:00.000Z\",\"Class\":\"Class A\",\"MMSI\":304858000,\"MsgType\":\"position_report\",\"Position\":{\"type\":\"Point\",\"coordinates\":[55.218332,13.371672]},\"Status\":\"Under way using engine\",\"SoG\":10.8,\"CoG\":94.3,\"Heading\":97},
                 {\"Timestamp\":\"2020-11-18T00:00:00.000Z\",\"Class\":\"AtoN\",\"MMSI\":992111840,\"MsgType\":\"static_data\",\"IMO\":\"Unknown\",\"Name\":\"WIND FARM BALTIC1NW\",\"VesselType\":\"Undefined\",\"Length\":60,\"Breadth\":60,\"A\":30,\"B\":30,\"C\":30,\"D\":30},
                 {\"Timestamp\":\"2020-11-18T00:00:00.000Z\",\"Class\":\"Class A\",\"MMSI\":219005465,\"MsgType\":\"position_report\",\"Position\":{\"type\":\"Point\",\"coordinates\":[54.572602,11.929218]},\"Status\":\"Under way using engine\",\"RoT\":0,\"SoG\":0,\"CoG\":298.7,\"Heading\":203},
@@ -228,22 +251,24 @@ class TMB_test(unittest.TestCase):
                 {\"Timestamp\":\"2020-11-18T00:00:00.000Z\",\"Class\":\"Class A\",\"MMSI\":376503000,\"MsgType\":\"position_report\",\"Position\":{\"type\":\"Point\",\"coordinates\":[54.519373,11.47914]},\"Status\":\"Under way using engine\",\"RoT\":0,\"SoG\":7.6,\"CoG\":294.4,\"Heading\":290},
                 {\"Timestamp\":\"2020-11-18T00:06:00.000Z\",\"Class\":\"Class A\",\"MMSI\":376503000,\"MsgType\":\"position_report\",\"Position\":{\"type\":\"Point\",\"coordinates\":[54.519373,11.47914]},\"Status\":\"Under way using engine\",\"RoT\":0,\"SoG\":7.6,\"CoG\":294.4,\"Heading\":290} ]"""
 
-    def test_insert_message_batch_interface_1( self  ):
+    def test_insert_message_batch_interface_1(self):
         """
         Function `insert_message_batch` takes an array of messages as an input.
         """
+        tmb = DAO(True)
         array = json.loads( self.batch )
-        inserted_count = self.tmb.insert_message_batch( array )
+        inserted_count = tmb.insert_message_batch( array )
         self.assertTrue( type(inserted_count) is  int and inserted_count >=0) 
 
-    def test_insert_message_batch_interface_2( self  ):
+    def test_insert_message_batch_interface_2(self):
         """
         Function `insert_message_batch` fails nicely if input is still a string
         """ 
-        inserted_count = self.tmb.insert_message_batch( self.batch )
+        tmb = DAO(True)
+        inserted_count = tmb.insert_message_batch( self.batch )
         self.assertEqual( inserted_count, -1) 
 
-    def test_insert_message_batch( self  ):
+    def test_insert_message_batch_integration(self):
         """
         Function `insert_message_batch` inserts messages in the MySQL table
         """
@@ -252,42 +277,57 @@ class TMB_test(unittest.TestCase):
         inserted_count = tmb.insert_message_batch( array )
         self.assertTrue( type(inserted_count) is  int and inserted_count >=0) 
 
-    def test_delete_old_message():
+    
+    def test_read_recent_position_given_MMSI_interface_fail(self):
+        MMSI = 1000
+        tmb = DAO(True)
+        value = tmb.read_recent_position_given_MMSI(MMSI)
+        self.assertTrue(value=="")
+    
+    def test_read_recent_position_given_MMSI_integration(self):
+        tmb=DAO()
+        result = tmb.read_recent_position_given_MMSI(219007155)
+        self.assertEqual(result, [('2020-11-18 00:00:00', '219007155', '54.947323', '11.107765', 'None')])
+
+
+    def test_read_all_recent_ship_positions_integration(self):
+        tmb=DAO()
+        result = tmb.read_all_recent_ship_positions()
+        self.assertEqual(result[2], ('265866000', '54.763183', '12.415067', '9217242'))
+
+    def test_read_last_five_positions_given_MMSI(self):
+        tmb=DAO()
+        result = tmb.read_last_five_positions_given_MMSI(219007155)
+        self.assertEqual(result[0], ('2020-11-18 00:00:00', '219007155', '54.947323', '11.107765', 'None'))
+
+    """
+    def test_read_vessel_info(self):
         pass
 
-    def test_read_recent_position_given_MMSI(self, MMSI):
+    def test_delete_old_message(self):
         pass
 
-    def test_read_all_recent_ship_positions(self):
+    def test_read_recent_positions_given_tile(self):
         pass
 
-    def test_read_vessel_info(self, MMSI, IMO, name):
+    def test_read_all_ports_matching_name(self):
         pass
 
-    def test_read_recent_positions_given_tile(self, tile_id):
+    def test_read_recent_positions_given_tile_and_port(self):
         pass
 
-    def test_read_all_ports_matching_name(self, port_name, country):
+    def test_read_recent_ship_positions_headed_to_port_ID(self):
         pass
 
-    def test_read_recent_positions_given_tile_and_port(self, port_name, country):
+    def test_read_recent_ship_positions_headed_to_port(self):
         pass
 
-    def test_read_last_five_positions_given_MMSI(self, MMSI):
+    def test_lookup_contained_tiles(self):
         pass
 
-    def test_read_recent_ship_positions_headed_to_port_ID(self, port_id):
+    def test_get_tile_PNG(self):
         pass
-
-    def test_read_recent_ship_positions_headed_to_port(self, port_name, country):
-        pass
-
-    def test_lookup_contained_tiles(self, tile_id):
-        pass
-
-    def test_get_tile_PNG(self, tile_id):
-        pass
-
+"""
     
 
 
